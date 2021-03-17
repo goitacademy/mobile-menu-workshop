5fe58046b8be4ba05708caca0dbd5cbcd54aae66
Alexander Repeta <alexander.repeta@gmail.com>
2021-03-17 18:18:01 +0200

    update script

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,23 @@
 (() => {
-  const menuContainer = document.querySelector('.js-menu-container');
+  const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => menuContainer.classList.toggle('is-open');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+  };
+
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  window
-    .matchMedia('(min-width: 600px)')
-    .addEventListener(
-      'change',
-      e => e.matches && menuContainer.classList.remove('is-open'),
-    );
+  window.matchMedia('(min-width: 600px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+  });
 })();
