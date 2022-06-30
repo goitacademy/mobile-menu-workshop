5a463f11b3f7822e442a3c95c5261d370975ebbe
Alexander Repeta <alexander.repeta@gmail.com>
2022-06-30 11:24:57 +0300

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -18,8 +18,7 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
+  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
